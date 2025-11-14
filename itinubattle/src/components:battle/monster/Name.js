bd395b65d7533dc95f7fce2e05cf3692a9aa2a2d
abekyoko <80676240+kyokoabe@users.noreply.github.com>
2025-11-14 10:35:11 +0900

    Delete itinubattle directory

--- itinubattle/src/components:battle/monster/Name.js
+++
@@ -1,8 +0,0 @@
-const Name = (props) => {
-    const { name, level, textAlign } = props;
-    return (
-        <span style={{ textAlign: textAlign }}>{name}：L{level}</span>
-    );
-}
-
-export default Name;
--- itinubattle/src/components:battle/utils/wait.js
+++
@@ -1,2 +0,0 @@
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-export default wait;
